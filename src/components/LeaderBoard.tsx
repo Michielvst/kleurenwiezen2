cee05a3540bd12b6ba27f80dbcb0f3fdb5ee6ea6
Michielvst <michiel--vs@outlook.com>
2020-03-26 11:01:31 +0100

    a

--- src/components/LeaderBoard.tsx
+++ src/components/LeaderBoard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 interface IProps {
   players: {
     [name: string]: {
@@ -9,23 +8,34 @@ interface IProps {
   };
 }
 
-const colors: {
-  [x: string]: string | undefined;
-} = {
-  1: "gold",
-  2: "silver",
-  3: "#cd7f32",
-  default: "transparant"
-};
-
 export default function LeaderBoard({ players }: IProps) {
-  const sortedPlayers = Object.entries(players)
-    .map(([key, value]) => ({
-      ...value,
-      name: key
-    }))
-    .sort((a, b) => b.totScore - a.totScore);
+  function topColors(rang: number): any {
+    if (rang === 1) {
+      return { backgroundColor: "gold" };
+    } else if (rang === 2) {
+      return { backgroundColor: "silver" };
+    } else if (rang === 3) {
+      return { backgroundColor: "#cd7f32" };
+    }
+  }
 
+  function renderLeaderBoard(): any {
+    const playersSorted = Object.keys(players).sort((a: any, b: any) => {
+      return players[b].totScore - players[a].totScore;
+    });
+    return playersSorted.map((el: string) => {
+      return (
+        <tr key={el}>
+          <td style={topColors(playersSorted.indexOf(el) + 1)}>
+            {playersSorted.indexOf(el) + 1}
+          </td>
+          <td>{el}</td>
+          <td>{players[el].totScore}</td>
+          <td>{players[el].gamesPlayed}</td>
+        </tr>
+      );
+    });
+  }
   return (
     <div className="leaderboard">
       <h2>Leaderboard</h2>
@@ -38,22 +48,7 @@ export default function LeaderBoard({ players }: IProps) {
             <th className="spellenGespeeld">Gespeeld</th>
           </tr>
         </thead>
-        <tbody>
-          {sortedPlayers.map((el, idx) => (
-            <tr key={el.name}>
-              <td
-                style={{
-                  backgroundColor: colors[idx + 1] || colors.default
-                }}
-              >
-                {idx + 1}
-              </td>
-              <td>{el.name}</td>
-              <td>{el.totScore}</td>
-              <td>{el.gamesPlayed}</td>
-            </tr>
-          ))}
-        </tbody>
+        <tbody>{renderLeaderBoard()}</tbody>
       </table>
     </div>
   );
